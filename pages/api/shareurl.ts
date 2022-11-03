376872c200340d1ea54c85b26f4bd6d14c33cbb8
teobaj <58204765+teobaj@users.noreply.github.com>
2022-11-03 10:23:16 +0200

    add rxjs

---
+++ pages/api/shareurl.ts
@@ -0,0 +1,11 @@
+import { url$ } from "../../services/url.service";
+export default async function handler(req, res) {
+    const {method , body} = req;
+    if(method === "POST"){
+        let obj = JSON.parse(body);
+        if(obj.url){
+            url$.next(obj.url)
+        }
+    }
+    res.status(200).json({url: url$.getValue()})
+}
